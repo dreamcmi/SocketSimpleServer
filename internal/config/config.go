624498ca113009cab80b3cdd001689a2e67dc2a8
dreamcmi <1912544842@qq.com>
2023-08-06 12:19:40 +0800

    fix log package path

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,8 +1,8 @@
 package config
 
 import (
+	"SocketSimpleServer/internal/log"
 	"github.com/BurntSushi/toml"
-	"github.com/rs/zerolog/log"
 	"net"
 )
 
@@ -30,24 +30,24 @@ func Parse() {
 		ip net.IP
 	)
 	if _, err := toml.DecodeFile("config.toml", &Config); err != nil {
-		log.Panic().Msgf("Decode config.toml Error: %v", err)
+		log.Log.Panic().Msgf("Decode config.toml Error: %v", err)
 	}
 
 	ip = net.ParseIP(Config.Tcp.Ip)
 	if ip == nil {
-		log.Panic().Msg("TCP ip config parse Error")
+		log.Log.Panic().Msg("TCP ip config parse Error")
 	}
 
 	ip = net.ParseIP(Config.Udp.Ip)
 	if ip == nil {
-		log.Panic().Msg("UDP ip config parse Error")
+		log.Log.Panic().Msg("UDP ip config parse Error")
 	}
 
 	if Config.Tcp.MaxSize <= 0 {
-		log.Panic().Msg("TCP maxSize <= 0")
+		log.Log.Panic().Msg("TCP maxSize <= 0")
 	}
 
 	if Config.Udp.MaxSize <= 0 {
-		log.Panic().Msg("UDP maxSize <= 0")
+		log.Log.Panic().Msg("UDP maxSize <= 0")
 	}
 }
